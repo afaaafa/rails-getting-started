0b44be1ad765a657a64d011fb6117f8c95a5c322
AF <af.arthurfaria@gmail.com>
2025-04-12 18:17:46 -0300

    style: rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,14 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :products do
-    resources :subscribers, only: [:create]
+    resources :subscribers, only: [ :create ]
   end
 
-  resource :unsubscribe, only: [:show]
+  resource :unsubscribe, only: [ :show ]
 
   root "products#index"
-
 end
