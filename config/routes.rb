06969f5e9f4dafc716887ccc9e3a2b3a29499623
AF <af.arthurfaria@gmail.com>
2025-04-12 17:34:49 -0300

    in-stock notifications

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,11 @@ Rails.application.routes.draw do
   
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :products
+  resources :products do
+    resources :subscribers, only: [:create]
+  end
 
-  # get "/products", to: "products#index"
-
-  # get "/products/new", to: "products#new"
-  # post "/products", to: "products#create"
-
-  # get "/products/:id", to: "products#show"
-
-  # get "/products/:id/edit", to: "products#edit"
-  # patch "/products/:id", to: "products#update"
-  # put "/products/:id", to: "products#update"
-
-  # delete "/products/:id", to: "products#destroy"
+  resource :unsubscribe, only: [:show]
 
   root "products#index"
 
